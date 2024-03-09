3049e22ed755d3b2aa50162d9b345d6ed2df6701
Your Name <you@example.com>
2024-03-09 22:34:46 +0200

    wip

--- main.go
+++ main.go
@@ -24,9 +24,9 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create snippet"))
