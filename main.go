e028ec34ef56fe303fc1d28b2ffd302a48d47d2f
Your Name <you@example.com>
2024-03-09 21:27:33 +0200

    fixed paths

--- main.go
+++ main.go
@@ -9,11 +9,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from this app"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display snippet"))
+
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create snippet"))
+}
+
 func main() {
 
 	// mux is like router synonym
 	mux := http.NewServeMux()
+	// subtree path, end with / (and starts on this case)
 	mux.HandleFunc("/", home)
+	// fixed path
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("start serving on  :4000")
 	err := http.ListenAndServe(":4000", mux)
